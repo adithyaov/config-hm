c76254fe34cb0ce2348d6c8281e0866470ff1f40
adithyaov <adi.obilisetty@gmail.com>
2020-12-09 13:45:28 +0530

    Add some functionality

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -77,6 +77,21 @@
 			:port "7997"
 			:home "/home/adithya")
 
+;; =============================================================================
+
+;; functionality
+(setq-default indent-tabs-mode nil)
+
+;; =============================================================================
+
+;; functionality
+
+;; bury *scratch* buffer instead of kill it
+(defadvice kill-buffer (around kill-buffer-around-advice activate)
+  (let ((buffer-to-kill (ad-get-arg 0)))
+    (if (equal buffer-to-kill "*scratch*")
+        (bury-buffer)
+      ad-do-it)))
 
 ;; =============================================================================
 
